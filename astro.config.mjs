c53c663683f7181263f50e32153c307d8b3387d6
Daniel Baranowski <daniel.baranowski@infinityworks.com>
2023-09-21 22:42:11 +0100

    try static

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,12 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 
-import vercel from "@astrojs/vercel/serverless";
+import vercelStatic from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx()],
   site: "https://wander-craver.com",
-  output: "server",
-  adapter: vercel(),
+  output: 'static',
+  adapter: vercelStatic(),
 });
